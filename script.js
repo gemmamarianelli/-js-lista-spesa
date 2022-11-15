76e4e1ad5676cc11340e4b0d9d2769bfe2dbe750
Gemma Marianelli <115584815+gemmamarianelli@users.noreply.github.com>
2022-11-15 17:20:41 +0100

    ultmio push

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
+let arrayProdotti = ["pane", "pasta", "pesce", "uova"];
 
-for(let i = 0; i < 1; i++){
-    let Prodotti = ["pasta","pane", "carne", "uova", "pesce"];
- console.log(Prodotti);
+let lista = document.getElementById("lista");
+
+//ciclo for
+for (let i = 0; i < 4; i++) {
+    console.log(arrayProdotti[i]);
+    lista.innerHTML += "<li>" + arrayProdotti[i] + "</li>";
+
 }
+
+//ciclo while
+
+while(i<arrayProdotti.length){
+    console.log(arrayProdotti[i]);
+}
+
+
